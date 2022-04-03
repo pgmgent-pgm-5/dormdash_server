19168e438ca2cbcbd1f050878ba77f3e9c99fc5d
pgm-sybrdebo <sybren.deboever@student.arteveldehs.be>
2022-04-03 18:18:45 +0200

    changed db to heroku

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -21,16 +21,17 @@ const nodeEnvironment = `${(
 
 const typeormConfig: ConnectionOptions = {
   type: 'postgres',
-  host: 'frankfurt-postgres.render.com',
+  host: 'ec2-176-34-211-0.eu-west-1.compute.amazonaws.com',
   port: 5432,
-  username: 'jamsy',
-  password: 'cB2YbWjtHSlHqNeVqUQgNTck4ZmG4VdQ',
-  database: 'dormdash',
+  username: 'kgxdduiaxdbszz',
+  password: 'e876c9082f8aa24bceec14f96c4123337476ea9b2e9032e3713f219a4426064c',
+  database: 'deq0nh04fturto',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
   logging: nodeEnvironment === 'development' ? true : false,
   dropSchema: nodeEnvironment === 'test' ? true : false,
-  ssl: true,
+  // ssl: true,
+  ssl: { rejectUnauthorized: false }
 };
 
 // important to work with CLI
